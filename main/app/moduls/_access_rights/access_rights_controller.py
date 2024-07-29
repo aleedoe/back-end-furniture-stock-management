0c2fed811d869a22ea20c030df11f06696f10982
alie <mushyafaali@gmail.com>
2024-07-29 15:24:03 +0700

    add modul access_rights

---
+++ main/app/moduls/_access_rights/access_rights_controller.py
@@ -0,0 +1,6 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
+from . import access_rights_model
+from . import access_rights_serializer
