830928f927d584b53539c728e3388d51cf913b4a
alie <mushyafaali@gmail.com>
2024-08-01 16:33:04 +0700

    create model

--- main/app/models.py
+++ main/app/models.py
@@ -1,1 +1,2 @@
 from app.moduls._access_rights import access_rights_model
+from app.moduls._administrator import administrator_model
--- main/app/moduls/_administrator/administrator_model.py
+++ main/app/moduls/_administrator/administrator_model.py
@@ -1,1 +1,16 @@
 from django.db import models
+from app.moduls._access_rights.access_rights_model import AccessRights
+
+class Administrator(models.Model):
+    class Meta:
+        db_table = '_administrator'
+    name = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    access_rights_id = models.ForeignKey(AccessRights, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
