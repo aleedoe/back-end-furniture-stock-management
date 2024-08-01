af643117f9d4c17c17183637893a0b0bd212e3c1
alie <mushyafaali@gmail.com>
2024-08-01 17:36:14 +0700

    make model

--- main/app/moduls/_image/image_model.py
+++ main/app/moduls/_image/image_model.py
@@ -1,1 +1,21 @@
 from django.db import models
+from app.moduls._furniture.furniture_model import Furniture
+
+class Image(models.Model):
+    class Meta:
+        db_table = '_image'
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class ImageContainer(models.Model):
+    class Meta:
+        db_table = '_image_container'
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    furniture = models.ForeignKey(Furniture, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
