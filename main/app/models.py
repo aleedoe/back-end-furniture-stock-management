a92e5e5c375adf569a4ba7382966dbc965d10e9f
alie <mushyafaali@gmail.com>
2024-08-01 16:54:34 +0700

    migrated

--- main/app/models.py
+++ main/app/models.py
@@ -1,3 +1,4 @@
 from app.moduls._access_rights import access_rights_model
 from app.moduls._administrator import administrator_model
 from app.moduls._warehouser import warehouser_model
+from app.moduls._reseller import reseller_model
