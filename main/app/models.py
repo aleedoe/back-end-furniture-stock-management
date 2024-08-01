bda9c65534b8b6cc27699e224bdf980eb8144373
alie <mushyafaali@gmail.com>
2024-08-01 17:59:03 +0700

    migrated

--- main/app/models.py
+++ main/app/models.py
@@ -4,3 +4,4 @@ from app.moduls._warehouser import warehouser_model
 from app.moduls._reseller import reseller_model
 from app.moduls._furniture import furniture_model
 from app.moduls._image import image_model
+from app.moduls._transaction import transaction_model
