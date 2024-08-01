a119e3ad91fbb6bf4076cf86bac068ec3ac7924f
alie <mushyafaali@gmail.com>
2024-08-01 15:13:25 +0700

    create model

--- main/app/moduls/_access_rights/access_rights_model.py
+++ main/app/moduls/_access_rights/access_rights_model.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Customer(models.Model):
+    class Meta:
+        db_table = '_access_rights'
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
