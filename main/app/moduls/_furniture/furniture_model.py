620c835ba1ffcad5afbad468bc03ec84f9873303
alie <mushyafaali@gmail.com>
2024-08-01 17:07:39 +0700

    make model

--- main/app/moduls/_furniture/furniture_model.py
+++ main/app/moduls/_furniture/furniture_model.py
@@ -1,2 +1,28 @@
 from django.db import models
+from app.moduls._warehouser import Warehouser
 
+class Category(models.Model):
+    class Meta:
+        db_table = '_category'
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Furniture(models.Model):
+    class Meta:
+        db_table = '_furniture'
+    name = models.CharField(max_length=255)
+    prize = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    description = models.TextField()
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    attributes = models.JSONField(null=True, blank=True)
+    warehouser_id = models.ForeignKey(Warehouser, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
