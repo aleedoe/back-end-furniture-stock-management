b26f0d3c3a9d8a811f1fce91f3a7d4c01239efa3
alie <mushyafaali@gmail.com>
2024-08-02 15:32:12 +0700

    add serializer reseller

--- main/app/moduls/_reseller/reseller_serializer.py
+++ main/app/moduls/_reseller/reseller_serializer.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 
 from . import reseller_model
+
+class ResellerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = reseller_model.Reseller
+        fields = ['id', 'name', 'phone', 'email', 'password', 'access_rights_id']
