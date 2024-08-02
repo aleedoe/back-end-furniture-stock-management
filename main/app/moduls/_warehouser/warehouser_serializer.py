131ef0dbac6b220deb25e5d446b1344158ed927f
alie <mushyafaali@gmail.com>
2024-08-02 15:54:11 +0700

    add serializer

--- main/app/moduls/_warehouser/warehouser_serializer.py
+++ main/app/moduls/_warehouser/warehouser_serializer.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 
 from . import warehouser_model
+
+class WarehouserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = warehouser_model.Warehouser
+        fields = ['id', 'name', 'phone', 'email', 'password', 'access_rights_id']
