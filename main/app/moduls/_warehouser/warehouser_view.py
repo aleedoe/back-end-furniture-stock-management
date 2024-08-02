4c2cd27f761c7e652b31db5d1e1f5ce4cd0b60fd
alie <mushyafaali@gmail.com>
2024-08-02 15:58:36 +0700

    add requst method get

--- main/app/moduls/_warehouser/warehouser_view.py
+++ main/app/moduls/_warehouser/warehouser_view.py
@@ -1,3 +1,8 @@
 from rest_framework.decorators import api_view
 
 from . import warehouser_controller
+
+@api_view(['GET', 'POST'])
+def warehouserList(request):
+    if request.method == 'GET':
+        return warehouser_controller.getAllWarehouser(request)
