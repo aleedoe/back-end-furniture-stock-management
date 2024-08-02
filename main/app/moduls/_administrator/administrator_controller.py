595f51b73b1e860e1d8466ecc91fbed11387579a
alie <mushyafaali@gmail.com>
2024-08-02 14:50:45 +0700

    add method for get all administrator

--- main/app/moduls/_administrator/administrator_controller.py
+++ main/app/moduls/_administrator/administrator_controller.py
@@ -4,3 +4,31 @@ from rest_framework.pagination import PageNumberPagination
 
 from . import administrator_model
 from . import administrator_serializer
+
+def getAllAdministrators(request):
+    
+    try:
+        administrators = administrator_model.Administrator.objects.all().order_by('id')
+        paginator = PageNumberPagination()
+        result_page = paginator.paginate_queryset(administrators, request)
+        serializer = administrator_serializer.AdministratorSerializer(result_page, many=True)
+
+        data_response = {
+            'status': "success",
+            'total_data': paginator.page.paginator.count,
+            'total_pages': paginator.page.paginator.num_pages,
+            'current_page': paginator.page.number,
+            'next': paginator.get_next_link(),
+            'previous': paginator.get_previous_link(),
+            'data': serializer.data,
+        }
+        
+        return Response(data_response)
+    
+    except Exception as e:
+        data_response = {
+            'status': "error",
+            'message': str(e),
+        }
+        
+        return Response(data_response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
