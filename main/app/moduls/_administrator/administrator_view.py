8ab2fb845f029e1460bfe9d5581a6794ff1ab1d1
alie <mushyafaali@gmail.com>
2024-08-02 14:54:13 +0700

    add method administratorList

--- main/app/moduls/_administrator/administrator_view.py
+++ main/app/moduls/_administrator/administrator_view.py
@@ -1,3 +1,10 @@
 from rest_framework.decorators import api_view
 
 from . import administrator_controller
+
+@api_view(['GET', 'POST'])
+def administratorList(request):
+    if request.method == 'GET':
+        return administrator_controller.getAllAdministrators(request)
+    
+    # elif request.method == 'POST':
