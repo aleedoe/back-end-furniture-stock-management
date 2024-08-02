e133ecd92f6a994b4c82774bcee07a288914c141
alie <mushyafaali@gmail.com>
2024-08-02 16:18:27 +0700

    add serializer category

--- main/app/moduls/_furniture/furniture_serializer.py
+++ main/app/moduls/_furniture/furniture_serializer.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 
 from . import furniture_model
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = furniture_model.Category
+        fields = ['id', 'name']
