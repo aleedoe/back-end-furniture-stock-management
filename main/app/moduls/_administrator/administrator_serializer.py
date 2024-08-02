e854d5eca80f32cc963a4f9a445919022664b9b4
alie <mushyafaali@gmail.com>
2024-08-02 14:49:40 +0700

    add serializer

--- main/app/moduls/_administrator/administrator_serializer.py
+++ main/app/moduls/_administrator/administrator_serializer.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 
 from . import administrator_model
+
+class AdministratorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = administrator_model.Administrator
+        # fields = ['id', 'customer_name', 'phone', 'email', 'password', 'total_orders']
