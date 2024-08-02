ff9d6fd194315927df9adf03379fd810656c34f8
alie <mushyafaali@gmail.com>
2024-08-02 16:44:35 +0700

    add metrhod for get all funiture

--- main/app/moduls/_furniture/furniture_controller.py
+++ main/app/moduls/_furniture/furniture_controller.py
@@ -32,3 +32,31 @@ def getAllCategories(request):
         }
         
         return Response(data_response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+def getAllFurnitures(request):
+    
+    try:
+        furnitures = furniture_model.Furniture.objects.all().order_by('id')
+        paginator = PageNumberPagination()
+        result_page = paginator.paginate_queryset(furnitures, request)
+        serializer = furniture_serializer.FurnitureSerializer(result_page, many=True)
+
+        data_response = {
+            'status': "success",
+            'total_data': paginator.page.paginator.count,
+            'total_pages': paginator.page.paginator.num_pages,
+            'current_page': paginator.page.number,
+            'next': paginator.get_next_link(),
+            'previous': paginator.get_previous_link(),
+            'data': serializer.data,
+        }
+        
+        return Response(data_response)
+    
+    except Exception as e:
+        data_response = {
+            'status': "error",
+            'message': str(e),
+        }
+        
+        return Response(data_response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
