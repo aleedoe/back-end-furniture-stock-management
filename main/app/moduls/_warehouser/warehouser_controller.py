d1f206a9cf3db29b960f003c26f7440f714aad33
alie <mushyafaali@gmail.com>
2024-08-02 15:55:53 +0700

    add method for get all reseller

--- main/app/moduls/_warehouser/warehouser_controller.py
+++ main/app/moduls/_warehouser/warehouser_controller.py
@@ -4,3 +4,31 @@ from rest_framework.pagination import PageNumberPagination
 
 from . import warehouser_model
 from . import warehouser_serializer
+
+def getAllReseller(request):
+    
+    try:
+        warehousers = warehouser_model.Warehouser.objects.all().order_by('id')
+        paginator = PageNumberPagination()
+        result_page = paginator.paginate_queryset(warehousers, request)
+        serializer = warehouser_serializer.WarehouserSerializer(result_page, many=True)
+
+        data_response = {
+            'status': "success",
+            'total_data': paginator.page.paginator.count,
+            'total_pages': paginator.page.paginator.num_pages,
+            'current_page': paginator.page.number,
+            'next': paginator.get_next_link(),
+            'previous': paginator.get_previous_link(),
+            'data': serializer.data,
+        }
+        
+        return Response(data_response)
+    
+    except Exception as e:
+        data_response = {
+            'status': "error",
+            'message': str(e),
+        }
+        
+        return Response(data_response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
