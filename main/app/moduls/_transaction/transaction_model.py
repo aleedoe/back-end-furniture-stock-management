4cda8540d19329dae89b7321c153d64152c91a22
alie <mushyafaali@gmail.com>
2024-08-05 09:28:19 +0700

    remove colums in transaction table

--- main/app/moduls/_transaction/transaction_model.py
+++ main/app/moduls/_transaction/transaction_model.py
@@ -13,8 +13,6 @@ class Transaction(models.Model):
     class Meta:
         db_table = '_transaction'
     reseller = models.ForeignKey(Reseller, on_delete=models.CASCADE)
-    furniture = models.ForeignKey(Furniture, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
     status = models.CharField(max_length=50, choices=[(status.name, status.value) for status in TransactionStatus])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
