bc82da444c40d64830df4d28a9c1732ff32819d7
alie <mushyafaali@gmail.com>
2024-08-07 20:30:20 +0700

    delete warehouser modul

--- main/app/moduls/_warehouser/warehouser_url.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import warehouser_view
-
-urlpatterns = [
-    path('warehouser/', warehouser_view.warehouserList),
-    path('warehouser/<int:id>/', warehouser_view.warehouser),
-]
