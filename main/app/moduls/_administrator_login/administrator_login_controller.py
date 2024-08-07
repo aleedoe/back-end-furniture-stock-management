bc489fc30e67c057680fae9d429be5db0b6b232a
alie <mushyafaali@gmail.com>
2024-08-07 21:56:07 +0700

    add error handling on administrator login

--- main/app/moduls/_administrator_login/administrator_login_controller.py
+++ main/app/moduls/_administrator_login/administrator_login_controller.py
@@ -12,30 +12,36 @@ def verifyLogin(request):
     try:
         username = request.data['username']
         password = request.data['password']
-        
-        administrator = Administrator.objects.get(Q(name=username) | Q(email=username), password=password)
-        
-        serializer = AdministratorSerializer(administrator)
 
+        # Cek apakah ada administrator dengan username atau email yang diberikan
+        try:
+            administrator = Administrator.objects.get(Q(name=username) | Q(email=username))
+        except Administrator.DoesNotExist:
+            data_response = {
+                'status': "error",
+                'message': "Username not found.",
+            }
+            return Response(data_response, status=status.HTTP_401_UNAUTHORIZED)
+        
+        # Verifikasi password
+        if administrator.password != password:
+            data_response = {
+                'status': "error",
+                'message': "Invalid password.",
+            }
+            return Response(data_response, status=status.HTTP_401_UNAUTHORIZED)
+        
+        # Jika username dan password valid
+        serializer = AdministratorSerializer(administrator)
         data_response = {
             'status': "success",
             'data': serializer.data,
         }
-        
         return Response(data_response, status=status.HTTP_200_OK)
-    
-    except Administrator.DoesNotExist:
-        data_response = {
-            'status': "error",
-            'message': "Invalid username or password.",
-        }
-        
-        return Response(data_response, status=status.HTTP_401_UNAUTHORIZED)
-    
+
     except Exception as e:
         data_response = {
             'status': "error",
             'message': str(e),
         }
-        
         return Response(data_response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
