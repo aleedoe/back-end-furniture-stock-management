a211e997685b1a59deb1d20d0feab26a0409dc5d
alie <mushyafaali@gmail.com>
2024-08-07 23:00:15 +0700

    fix bug user not found when taking API

--- main/main/settings.py
+++ main/main/settings.py
@@ -134,7 +134,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTStatelessUserAuthentication',
     ),
 }
 
